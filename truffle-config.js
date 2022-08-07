1bff47e6e149ed7f83e399ba12bcc0f46e5f7a42
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-08-07 12:55:38 +0700

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 require("babel-register");
 require("babel-polyfill");
+require('dotenv').config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
@@ -8,6 +9,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*", // Match any network id
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(
+        process.env.PRIVATE_KEY, 
+        process.env.INFURA_URL
+      ),
+      network_id: 56,       //mainnet
     },
   },
 
@@ -20,4 +28,10 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    bscscan: ''
+  }
 };
